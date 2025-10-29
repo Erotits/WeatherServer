1c9b720b163cb7a9fc17bade8e80e57dbc3f9992
Erotits <baptiste.morel29@laposte.net>
2025-10-29 19:13:20 +0100

    2 versioner HTTPServerConnection_TaskWork

--- libs/HTTPServer/HTTPServerConnection.c
+++ libs/HTTPServer/HTTPServerConnection.c
@@ -44,12 +44,109 @@ void HTTPServerConnection_SetCallback(HTTPServerConnection* _Connection, void* _
 	_Connection->onRequest = _OnRequest;
 }
 
+void HTTPServerConnection_TaskWork(void *_Context, uint64_t _MonTime)
+{
+    HTTPServerConnection *_Connection = (HTTPServerConnection *)_Context;
+
+    if (_Connection == NULL)
+        return;
+
+    _Connection->method = NULL;
+    _Connection->url = NULL;
+
+    char buffer[1024];
+
+    int bytesRead = TCPClient_Read(&_Connection->tcpClient, (uint8_t *)buffer, sizeof(buffer));
+    printf("%i\n", bytesRead);
+    if (bytesRead > 0)
+    {
+        buffer[bytesRead] = '\0';
+        char *ptr = &buffer[0];
+        char *space = strchr(ptr, ' ');
+        if (space != NULL)
+        {
+            int method_length = space - ptr;
+
+            _Connection->method = strndup(ptr, method_length);
+            if (strcmp(_Connection->method, "GET") == 0)
+                printf("%s\n", _Connection->method);
+        }
+
+        char *host = strstr(ptr, "Host: ");
+        if (host != NULL)
+        {
+            host += strlen("Host: ");
+
+            char *eol = strstr(host, "\r\n");
+
+            if (eol != NULL)
+            {
+                int url_length = eol - host;
+
+                _Connection->url = strndup(host, url_length);
+                printf("%s\n", _Connection->url);
+            }
+        }
+    }
+
+
+        if (_Connection->method != NULL && _Connection->url != NULL && (strcmp(_Connection->method, "GET") == 0))
+    {
+        printf("Method: %s\nURL: %s\n", _Connection->method, _Connection->url);
+        
+        if(strstr(buffer, "\r\n\r\n"))
+        _Connection->onRequest(_Connection->context);
+    }
+    
+}
+
+
+
+
+/* Second solution
 void HTTPServerConnection_TaskWork(void* _Context, uint64_t _MonTime)
 {
-	//HTTPServerConnection* _Connection = (HTTPServerConnection*)_Context;
-	
-	printf("HTTPServerConnection_TaskWork\n");
+	HTTPServerConnection* _Connection = (HTTPServerConnection*)_Context;
+
+	if(_Connection == NULL)
+	{
+		return;
+	}
+	char buffer[1024];
+
+	int bytesRead = TCPClient_Read(&_Connection->tcpClient, (uint8_t *)buffer, sizeof(buffer));
+
+	if(bytesRead > 0)
+	{
+		
+		buffer[bytesRead] = '\0';
+		char* ptr = &buffer[0];
+		
+		if(ptr == NULL)
+		{
+			return;
+		}
+		char* line = strndup(ptr, *strchr(buffer, ' '));
+		
+		char* pch = strtok(line, " ");
+		_Connection->method = strdup(pch);
+		
+		pch = strtok(NULL, "\r\n");
+		_Connection->url =  strdup(pch);
+	}
+	else
+	{
+		return;
+	}	
+	if (_Connection->method != NULL && _Connection->url != NULL && (strcmp(_Connection->method, "GET") == 0))
+    {
+        printf("Method: %s\nURL: %s\n", _Connection->method, _Connection->url);
+        
+        if(strstr(buffer, "\r\n\r\n"))
+        	_Connection->onRequest(_Connection->context);
+    }
 }
+*/
 
 void HTTPServerConnection_Dispose(HTTPServerConnection* _Connection)
 {
